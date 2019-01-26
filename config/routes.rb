7f86f8eae0b8cda238a5f63bc9a8d3d92b535f02
iwasawa15 <tatsuya57sak@gmail.com>
2019-01-26 16:13:20 +0900

    create groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'groups/new'
+
+  get 'groups/create'
+
+  get 'groups/edit'
+
+  get 'groups/update'
+
   devise_for :users
   resources :users, only: [:edit, :update]
 
