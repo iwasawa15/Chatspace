08e80d88a1e1076e58111b15af236602051618a6
iwasawa15 <tatsuya57sak@gmail.com>
2019-01-26 16:27:31 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'groups/new'
-
-  get 'groups/create'
-
-  get 'groups/edit'
-
-  get 'groups/update'
-
   devise_for :users
-  resources :users, only: [:edit, :update]
-
-  resources :messages, only: [:index]
-
-  root 'messages#index'
+  root "groups#index"
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
