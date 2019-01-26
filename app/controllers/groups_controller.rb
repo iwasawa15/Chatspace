4d5c39e5e4bd4671891ffbcea2389c757ce46288
iwasawa15 <tatsuya57sak@gmail.com>
2019-01-26 17:22:01 +0900

    new create edit update action at groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,35 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def new
+    @group = Group.new
   end
 
   def create
+    if Group.create(group_params)
+      redirect_to :root, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @group.udpate(group_params)
+      redirect_to :root, notice: "グループを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
