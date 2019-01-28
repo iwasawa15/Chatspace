521a8995f5c95d5c093da6ad778f16990033bf97
iwasawa15 <tatsuya57sak@gmail.com>
2019-01-29 02:57:11 +0900

    currect create action at groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-    if Group.create(group_params)
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to :root, notice: "グループを作成しました"
     else
       render :new
