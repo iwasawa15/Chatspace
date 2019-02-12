582fa8d16650d7c837b34ab0f2f5891b7e0d14c5
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-12 19:07:49 +0900

    clear interval

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,82 +1,89 @@
-$(function(){
-  function buildHTML(message){
-    if (message.image){
-      var html = `<div class="messages__box" data-message-id=${message.id}>
-                    <div class="messages__name">
-                      ${message.user_name}
-                    </div>
-                    <div class="messages__time">
-                      ${message.date}
-                    </div>
-                    <div class="messages__text">
-                      ${message.body}
-                    </div>
-                    <img src=${message.image}>
-                  </div>`
-    } else {
-      var html = `<div class="messages__box" data-message-id=${message.id}>
-                    <div class="messages__name">
-                      ${message.user_name}
-                    </div>
-                    <div class="messages__time">
-                      ${message.date}
-                    </div>
-                    <div class="messages__text">
-                      ${message.body}
-                    </div>
-                  </div>`
-    }
-    return html;
-  }
-
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.messages').animate({scrollTop: $('.messages').height()}, 'fast')
-      $('.text-field').val('')
-      $('.upload-file').val('')
-    })
-    .fail(function(){
-      alert('error');
-    })
-  return false;
-  })
-
-  function update(){
-    var message_id = $('.messages__box:last').data('message-id');
-    $.ajax({
-      url: location.href,
-      type: "GET",
-      dataType: 'json',
-      data: {
-        message_id: message_id
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(message){
+      if (message.image){
+        var html = `<div class="messages__box" data-message-id=${message.id}>
+                      <div class="messages__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="messages__time">
+                        ${message.date}
+                      </div>
+                      <div class="messages__text">
+                        ${message.body}
+                      </div>
+                      <img src=${message.image}>
+                    </div>`
+      } else {
+        var html = `<div class="messages__box" data-message-id=${message.id}>
+                      <div class="messages__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="messages__time">
+                        ${message.date}
+                      </div>
+                      <div class="messages__text">
+                        ${message.body}
+                      </div>
+                    </div>`
       }
+      return html;
+    }
+
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $('.messages').animate({scrollTop: $('.messages').height()}, 'fast')
+        $('.text-field').val('')
+        $('.upload-file').val('')
+      })
+      .fail(function(){
+        alert('error');
+      })
+    return false;
     })
 
-    .always(function(data){
-      if(data.length != 0){
-        data.forEach(function(message){
-          var html = buildHTML(message);
-          $('.messages').append(html)
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
-        });
-      };
-    })
-  }
+    function update(){
+      var message_id = $('.messages__box:last').data('message-id');
+      if (location.href.match("/messages")){
+        $.ajax({
+          url: location.href,
+          type: "GET",
+          dataType: 'json',
+          data: {
+            message_id: message_id
+          }
+        })
 
-  setInterval(update, 5 * 1000);
+        .always(function(data){
+        if(data.length != 0){
+          data.forEach(function(message){
+            var html = buildHTML(message);
+            $('.messages').append(html)
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
+          });
+        };
+      })
+      } else {
+        clearInterval(timer);
+      }
+    }
 
+    if(location.href.match("/messages")){
+      var timer = setInterval(update, 5 * 1000);
+    }
+  })
 })
