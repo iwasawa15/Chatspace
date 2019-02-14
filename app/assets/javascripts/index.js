ef79db2e0dd79c7f1cb2d15ff720080d0202ea98
iwasawa15 <tatsuya57sak@gmail.com>
2019-02-14 17:13:02 +0900

    rename function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   $(function(){
     var search_list = $("#user-search-result");
     var chat_members = $("#chat-group-users")
-    function appendUser(user) {
+    function appendSearchUserResult(user) {
       var user = `<div class="chat-group-user clearfix">
                       <p class="chat-group-user__name">${user.name}</p>
                       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -32,7 +32,7 @@ $(document).on('turbolinks:load', function(){
       .done(function(users){
         $("#user-search-result").empty()
         users.forEach(function(user){
-          appendUser(user);
+          appendSearchUserResult(user);
         });
       })
       .fail(function(){
